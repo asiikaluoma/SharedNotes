99417ef178fac233e5e521994edcb6f0526a736b
alex.siikaluoma <asiikaluoma>
2019-09-13 16:27:46 +0300

    Edit and delete note functionality

--- app/notes/views.py
+++ app/notes/views.py
@@ -8,7 +8,7 @@ def notebook_notes(notebook_id):
     return render_template("notes/list.html", notes=Note.query.order_by(desc(Note.date_modified)).all(), notebook_id=notebook_id)
 
 @app.route("/notebook/<notebook_id>/notes/<note_id>", methods=["GET"])
-def notes(notebook_id, note_id):
+def notes_view(notebook_id, note_id):
     return render_template("notes/index.html", note=Note.query.get(note_id))
 
 @app.route("/notebook/<notebook_id>/notes/new")
@@ -23,3 +23,24 @@ def notes_create(notebook_id):
     db.session().commit()
   
     return redirect(url_for("notebook_notes", notebook_id=notebook_id))
+
+@app.route("/notebook/<notebook_id>/notes/<note_id>/delete", methods=["POST"])
+def notes_delete(notebook_id, note_id):
+    Note.query.filter_by(id=note_id).delete()
+    db.session().commit()
+  
+    return redirect(url_for("notebook_notes", notebook_id=notebook_id))
+
+
+@app.route("/notebook/notes/<note_id>/edit")
+def notes_new_edit(note_id):
+    return render_template("notes/edit.html", note=Note.query.get(note_id))
+
+@app.route("/notebook/notes/<note_id>/save", methods=["POST"])
+def notes_edit(note_id):
+    note = Note.query.get(note_id)
+    note.title = request.form.get("title")
+    note.body = request.form.get("body")
+    db.session().commit()
+
+    return redirect(url_for('notes_view', notebook_id=1, note_id=note_id))
