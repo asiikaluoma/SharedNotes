dcaeb447127584a09ec0f1c5045ae97cd27133f5
asiikaluoma <alec.siikaluoma@helsinki.fi>
2019-09-30 18:45:34 +0300

    Better form validatioN

--- app/notes/forms.py
+++ app/notes/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators, TextAreaField
 
 class NoteForm(FlaskForm):
-    title = StringField("Note title", [validators.Length(min=1)])
-    body = TextAreaField("Body", [validators.Length(min=1)])
+    title = StringField("Note title", [validators.Length(min=1, max=100)])
+    body = TextAreaField("Body", [validators.Length(min=1, max=10000)])
  
     class Meta:
         csrf = False
